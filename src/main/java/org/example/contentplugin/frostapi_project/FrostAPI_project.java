e3556f84dcb6a2c8e546870b82248b2898dbf78c
frostpyro <143861009+frostpyro2@users.noreply.github.com>
2026-04-10 16:44:02 +0900

    sdfa

---
+++ src/main/java/org/example/contentplugin/frostapi_project/FrostAPI_project.java
@@ -0,0 +1,17 @@
+package org.example.contentplugin.frostapi_project;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FrostAPI_project extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
